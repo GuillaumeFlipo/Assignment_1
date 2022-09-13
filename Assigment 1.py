1f968c8f59801f9be03ab58115ca292324dac61e
GuillaueFlipo <flipoguigui@gmail.com>
2022-09-13 10:49:47 +0200

    task 1.1

--- Assigment 1.py
+++ Assigment 1.py
@@ -99,10 +99,50 @@ def modelFunction():
 def task1_2_a():
 
     fileAar = pd.read_csv('aar.csv', sep=",",decimal=".", encoding='latin-1')
+    file = pd.read_csv("CSO_Mollea.csv", sep=",")
+    file.rename(columns={'Vandmængd': 'Water_volume', "Antal over": "Nb_overflow", "Bygværkst": "Building_type"},
+                inplace=True)
     nodeInput = fileAar[fileAar["beregningspunktlokalid"] == "Novana_Model_MOELLEAA_DK1_4083.0"]
-    print(nodeInput)
     meanFlow = nodeInput["vandfoering"].mean()
     print(meanFlow)
+    water_volume_input = meanFlow*3600*24*365.25
+    water_volume_CSO = file.Water_volume.sum()
+    print(file)
+    print(water_volume_CSO)
+    print(water_volume_input)
+    return file
+
+
+
+def task1_1():
+    file = pd.read_csv("CSO_Mollea.csv", sep=",")
+    file.rename(columns={'Vandmængd': 'Water_volume', "Antal over": "Nb_overflow", "Bygværkst": "Building_type"},
+                inplace=True)
+
+    indexOverflow = file[file["Nb_overflow"] > 0].index
+    Nb_CS0_overflow = len(indexOverflow)
+    NameCSOMostOverflow = file.iloc[file['Nb_overflow'].idxmax()].Navn
+    meanDischarge = file.Water_volume.mean()
+    medianDischarge = file.Water_volume.median()
+    # plt.hist(x=file.Water_volume.values, bins=30)
+    # plt.xlabel('Volume of water discharged (m^3)')
+    # plt.ylabel('CSO structures (Dimensionless)')
+    # plt.title('Distribution of CSO water volumes')
+
+    fileMaaned = pd.read_csv('maaned.csv', sep=",", decimal=".", encoding='latin-1')
+    fileAar = pd.read_csv('aar.csv', sep=",", decimal=".", encoding='latin-1')
+    fileBereg = pd.read_csv('bereg.csv', sep=",", decimal=".", encoding='latin-1')
+
+    nodeRiverMoelleaBereg = fileBereg[fileBereg["straekningsnavn"].str.contains("MOELLEAA")].reset_index(drop=True)
+    nodeRiverMoelleaMaaned = fileMaaned[fileMaaned["beregningspunktlokalid"].str.contains("MOELLEAA")].reset_index(
+        drop=True)
+    nodeRiverMoelleaAar = fileAar[fileAar["beregningspunktlokalid"].str.contains("MOELLEAA")].reset_index(drop=True)
+
+    # Average flow since 2010
+
+    AverageFlow = nodeRiverMoelleaAar[nodeRiverMoelleaAar['aar'] > 2010].vandfoering.mean()
+    nodeRiverMoelleaMaanedFilter = nodeRiverMoelleaMaaned[nodeRiverMoelleaMaaned['aar'] > 2010].reset_index(drop=True)
+    df_maaned_mean = nodeRiverMoelleaMaanedFilter.groupby(["maaned"]).vandfoering.mean()
 
 
 
@@ -113,46 +153,12 @@ def task1_2_a():
 
 if __name__ == '__main__':
 
-    #task1_2_a()
+    file = task1_2_a()
 
-    df = modelFunction()
+    #df = modelFunction()
 
-    # file = pd.read_csv("CSO_Mollea.csv", sep=",")
-    # file.rename(columns={'Vandmængd': 'Water_volume', "Antal over": "Nb_overflow", "Bygværkst": "Building_type"}, inplace=True)
-    #
-    # indexOverflow = file[file["Nb_overflow"] > 0].index
-    # Nb_CS0_overflow = len(indexOverflow)
-    # NameCSOMostOverflow = file.iloc[file['Nb_overflow'].idxmax()].Navn
-    # meanDischarge = file.Water_volume.mean()
-    # medianDischarge = file.Water_volume.median()
-    # # plt.hist(x=file.Water_volume.values, bins=30)
-    # # plt.xlabel('Volume of water discharged (m^3)')
-    # # plt.ylabel('CSO structures (Dimensionless)')
-    # # plt.title('Distribution of CSO water volumes')
-    #
-    # fileMaaned = pd.read_csv('maaned.csv', sep=",",decimal=".", encoding='latin-1')
-    # fileAar = pd.read_csv('aar.csv', sep=",",decimal=".", encoding='latin-1')
-    # fileBereg = pd.read_csv('bereg.csv', sep=",",decimal=".", encoding='latin-1')
-    #
-    #
-    # nodeRiverMoelleaBereg = fileBereg[fileBereg["straekningsnavn"].str.contains("MOELLEAA")].reset_index(drop=True)
-    # nodeRiverMoelleaMaaned = fileMaaned[fileMaaned["beregningspunktlokalid"].str.contains("MOELLEAA")].reset_index(drop=True)
-    # nodeRiverMoelleaAar = fileAar[fileAar["beregningspunktlokalid"].str.contains("MOELLEAA")].reset_index(drop=True)
-    #
-    #
-    # # Average flow since 2010
-    #
-    # AverageFlow = nodeRiverMoelleaAar[nodeRiverMoelleaAar['aar'] > 2010].vandfoering.mean()
-    # nodeRiverMoelleaMaanedFilter = nodeRiverMoelleaMaaned[nodeRiverMoelleaMaaned['aar'] > 2010].reset_index(drop=True)
-    # df_maaned_mean = nodeRiverMoelleaMaanedFilter.groupby(["maaned"]).vandfoering.mean()
-    #
-    # CSOdata = pd.read_csv('bereg.csv', sep=",", decimal=".", encoding='latin-1')
-    # RiverData = fileMaaned.copy()
-    # RiverData = RiverData[RiverData['beregningspunktlokalid'].str.contains("MOELLEAA")].reset_index(drop=True)
-    # RiverDataYY = RiverData[RiverData['aar'] == 2019 ].reset_index(drop=True)
-    # RiverDataMM = importRiverDataMonth("januar", RiverDataYY)
-    # indexUp = RiverDataMM['beregningspunktlokalid'  == "Novana_Model_MOELLEAA_DK1_3687.0"]
-    # indexDown = RiverDataMM['beregningspunktlokalid'  == "Novana_Model_MOELLEAA_DK1_13500.0"]
+
+
 
 
 
