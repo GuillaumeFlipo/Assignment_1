955da12249b65bd33eb3412ee156bae93fe6b973
Loann <loann.lameau@hotmail.fr>
2022-09-19 15:31:59 +0200

    fixed error in sensitivity

--- sensitivity.py
+++ sensitivity.py
@@ -7,6 +7,10 @@ def sensitivity_analysis(C0, CS0_conc) :
     EQS = 2
     dParam = np.arange(5,30,5)/100
     result = np.zeros((2,2*len(dParam)))
+    
+    base_C_C0 = np.max(modelFunction(C0, CS0_conc, EQS)["SimConcentration"])
+    base_C_CS0 = np.max(modelFunction(C0, CS0_conc, EQS)["SimConcentration"])
+    
     for i in range(len(dParam)) :
         factor = dParam[i]
         
@@ -15,12 +19,12 @@ def sensitivity_analysis(C0, CS0_conc) :
         
         max_C_C0_minus = np.max(modelFunction(C0*(1-factor), CS0_conc, EQS)["SimConcentration"])
         max_C_CS0_minus = np.max(modelFunction(C0, CS0_conc*(1-factor), EQS)["SimConcentration"])
+            
+        result[0,len(dParam)+i] = (max_C_C0-base_C_C0)/(C0*(1+factor))
+        result[1,len(dParam)+i] = (max_C_CS0-base_C_CS0)/(CS0_conc*(1+factor))
         
-        result[0,len(dParam)+i] = (max_C_C0-C0)/(C0*(1+factor))
-        result[1,len(dParam)+i] = (max_C_CS0-CS0_conc)/(CS0_conc*(1+factor))
-        
-        result[0,len(dParam)-i-1] = (max_C_C0_minus-C0)/(C0*(1-factor))
-        result[1,len(dParam)-i-1] = (max_C_CS0_minus-CS0_conc)/(CS0_conc*(1-factor))
+        result[0,len(dParam)-i-1] = (max_C_C0_minus-base_C_C0)/(C0*(1-factor))
+        result[1,len(dParam)-i-1] = (max_C_CS0_minus-base_C_CS0)/(CS0_conc*(1-factor))
         
     dParam_rev = dParam.copy()
     dParam_rev = -np.flip(dParam_rev)
