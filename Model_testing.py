900d023627ad3b1984355ba8e5ddb4f36562bacd
Loann <loann.lameau@hotmail.fr>
2022-09-22 15:23:09 +0200

    model testing update

--- Model_testing.py
+++ Model_testing.py
@@ -7,7 +7,7 @@ from model import modelFunction
 
 def model_testing() :
     ## test 1 : Concentration decrease without CSO discharge ##
-    Test1 = modelFunction(C0=1,CS0_conc=0)
+    Test1 = modelFunction(C0=1,CS0_conc=0,coef_V_CSO=0)
     test1_conc = Test1["SimConcentration"]
     
     dist = Test1["Distance"]
@@ -21,7 +21,15 @@ def model_testing() :
 
     plt.figure()
     plt.plot(dist,test2_conc)
-    plt.title("test 2 : Important concentration increase when there is a CSO discharge")
+    plt.title("test 2 : Concentration increase when there is a CSO discharge")
+    
+    ## test 3 : Simulation with real values ##
+    Test3 = modelFunction(C0=0,CS0_conc=1.8)
+    test3_conc = Test3["SimConcentration"]
+
+    plt.figure()
+    plt.plot(dist,test3_conc)
+    plt.title("test 3 : Simulation with real values")
     
     plt.show()
     
